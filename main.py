cffbf9dc89ede07f74a59d3a14ce938f4827ca71
Emon <md.emon18@st.niituniversity.in>
2022-02-03 22:35:36 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+patient = ["John Smith", "jhon"]
+
+age = 21
+is_new = True
+print(patient[0])
+patient = input("New patient name ")
+#age = input("patient age ")
+
+#print("hi " + str(is_new))
+first = input("first ")
+Second = input("Second ")
+sum = float(first) + int(Second)
+print("Sum:" + str(sum))
